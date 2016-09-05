facf6beab5154f7b395b040f421fb2edf4d4c433
王云 <wangyun120411@163.com>
2016-09-06 01:00:35 +0800

    添加箭头和label && 定义刷新的三种状态的枚举

--- sinaweibo/Classes/View/Home/View/WYRefreshControlH.swift
+++ sinaweibo/Classes/View/Home/View/WYRefreshControlH.swift
@@ -8,7 +8,20 @@
 
 import UIKit
 
+/// 定义刷新控件的三种状态
+///
+/// - normal:     <#normal description#>
+/// - pulling:    <#pulling description#>
+/// - refreshing: <#refreshing description#>
+enum WYRefreshType: String {
+    case normal = "normal"      // 默认状态，没有拖动或者拖动的没有将当前控件完全显示出来
+    case pulling = "pulling"    // 松手就可以刷新的状态，代表当前控件已经完全显示出来了
+    case refreshing = "refreshing"  // 刷新中的状态
+}
+
+
 private let HMRefreshControlH: CGFloat = 50
+
 class WYRefreshControl: UIControl {
 
     override init(frame: CGRect) {
@@ -25,6 +38,34 @@ class WYRefreshControl: UIControl {
         backgroundColor = UIColor.orange
         frame.size = CGSize(width: HMScreenW, height: HMRefreshControlH)
         frame.origin.y = -HMRefreshControlH
+        
+        //添加下拉刷新的控件
+        addSubview(arrowIcon)
+        addSubview(messageLabel)
+        
+        //约束
+        arrowIcon.snp_makeConstraints { (make ) in
+            make.centerX.equalTo(self).offset(-35)
+            make.centerY.equalTo(self)
+        }
+        messageLabel.snp_makeConstraints { (make ) in
+            make.leading.equalTo(arrowIcon.snp_trailing)
+            make.centerY.equalTo(arrowIcon)
+        }
+        
+        
     }
+    
+    
+    
+    //懒加载控件
+    lazy var arrowIcon : UIImageView = UIImageView(image: UIImage(named:"tableview_pull_refresh"))
+
+    lazy var messageLabel :UILabel = {
+        
+        let messageLabel = UILabel(textColour: UIColor.black, fontSize: 12)
+        messageLabel.text = "等待起飞"
+        return messageLabel
+    }()
 
 }
